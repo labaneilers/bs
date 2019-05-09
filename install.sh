450bf85d8afd919865a0128edda21df8d1e73b5c
Laban Eilers <leilers@firstrepublic.com>
2019-05-09 09:53:45 -0400

    Remove unused copy

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ cd $(dirname "$0")
 
 mkdir -p ~/bin
 curl -s -o ~/bin/bs https://raw.githubusercontent.com/labaneilers/bs/master/bs
-cp ./bs ~/bin
 chmod +x ~/bin/bs
 
 echo "bs installed at ~/bin/bs"
