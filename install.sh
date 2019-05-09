1aad91fe58964dbf6b2515b0c1389fbd7a469145
Laban Eilers <leilers@firstrepublic.com>
2019-05-09 13:39:20 -0400

    Improved unix install

--- install.sh
+++ install.sh
@@ -8,4 +8,10 @@ mkdir -p ~/bin
 curl -s -o ~/bin/bs https://raw.githubusercontent.com/labaneilers/bs/master/bs
 chmod +x ~/bin/bs
 
-echo "bs installed at ~/bin/bs"
+TESTED=$(which bs)
+if [[ "$TESTED" = "" ]]; then
+    echo "bs was installed at ~/bin/bs, but it wasn't on your PATH."
+    exit 1
+fi
+
+echo "bs installed successfully at ~/bin/bs"
