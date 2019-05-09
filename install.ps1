62a11dfac342ea9cfd5fa816d554599d23bad2c7
Laban Eilers <leilers@firstrepublic.com>
2019-05-09 13:33:16 -0400

    Improved windows install

--- install.ps1
+++ install.ps1
@@ -1,2 +1,16 @@
-iwr -uri https://raw.githubusercontent.com/labaneilers/bs/master/bs.cmd -outfile "C:\Program Files\Git\cmd\bs.cmd"
-iwr -uri https://raw.githubusercontent.com/labaneilers/bs/master/bs -outfile "C:\Program Files\Git\cmd\bs.sh"
+$target_dir = $env:BS_DIR
+if (! $env:BS_DIR) {
+    $target_dir = "C:\Program Files\Git\cmd"
+}
+$source = "https://raw.githubusercontent.com/labaneilers/bs/master"
+
+if (!( test-path $target_dir)) {
+    "ERROR: Couldn't find default install dir: $target_dir"
+    "You can set the target install dir by setting an environment variable: `$env:BS_DIR"
+    exit 1
+}
+
+iwr -uri "$source/bs.cmd" -outfile "$target_dir\bs.cmd"
+iwr -uri "$source/bs" -outfile "$target_dir\bs.sh"
+
+"Installed bs at $target_dir\bs.cmd"
