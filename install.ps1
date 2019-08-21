abca4a9fe3b7b395a57b8731576fb9c5c29d4128
Shapiro, Matt <mshapiro@firstrepublic.com>
2019-08-21 10:42:22 -0400

    Support non-global git installs

--- install.ps1
+++ install.ps1
@@ -1,15 +1,38 @@
-$target_dir = $env:BS_DIR
-if (! $env:BS_DIR) {
-    $target_dir = "C:\Program Files\Git\cmd"
+if (Get-Command git -ErrorAction SilentlyContinue) {
+    $git_path = (Get-Command git) | Split-Path | Split-Path
 }
+
+$possible_paths = @($env:BS_DIR, "$($git_path)\cmd")
+$non_empty_paths = $possible_paths | where {$_}
+$target_dir = $non_empty_paths |    
+    where {(Test-Path $_)} |
+    select -First 1
+
+if (!($target_dir)) {
+    $checkedPaths = $non_empty_paths | foreach {$paths} {$paths += "'$_'`r`n"} {$paths}
+
+    Write-Error @"
+Couldn't find BS install path.  Checked: 
+$checkedPaths
+You can manually set the target install dir by setting an environment variable: `$env:BS_DIR
+"@
+    return
+}
+
 $source = "https://raw.githubusercontent.com/labaneilers/bs/master"
+iwr -uri "$source/bs" -outfile "$target_dir\bs.sh"
 
-if (!( test-path $target_dir)) {
-    "ERROR: Couldn't find default install dir: $target_dir"
-    "You can set the target install dir by setting an environment variable: `$env:BS_DIR"
-} else {
-    iwr -uri "$source/bs.cmd" -outfile "$target_dir\bs.cmd"
-    iwr -uri "$source/bs" -outfile "$target_dir\bs.sh"
-
-    "Installed bs at $target_dir\bs.cmd"
+# Write customized cmd file pointing to git's bash executable
+$bash_path = "$git_path\bin\sh.exe"
+if (!(Test-Path $bash_path)) {
+    Write-Error "Could not find bash executable (tried '$bash_path')"
 }
+
+$cmd_path = "$target_dir\bs.cmd"
+rm -Force $cmd_path
+
+Set-Content -Path $cmd_path -Value "@echo off"
+Set-Content -Path $cmd_path -Value "SET SCRIPT_DIR=%~dp0"
+Set-Content -Path $cmd_path -Value "`"$bash_path`" --login -i -- `"%SCRIPT_DIR%bs.sh`" %*"
+
+"Installed bs at $target_dir\bs.cmd"
