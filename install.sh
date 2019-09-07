d3cb1e9280fccc72ce252115b4788233598f34d8
Laban Eilers <leilers@firstrepublic.com>
2019-09-07 17:42:33 -0400

    Make install path configurale

--- install.sh
+++ install.sh
@@ -2,20 +2,26 @@
 
 cd $(dirname "$0")
 
+if [ "$BS_DIR" = "" ]; then
+    BS_DIR="~/bin"
+fi
+
+BS_BIN_PATH="$BS_DIR/bs"
+
 mkdir -p ~/bin
-curl -s -f -S -o ~/bin/bs https://raw.githubusercontent.com/labaneilers/bs/master/bs
+curl -s -f -S -o "$BS_BIN_PATH" https://raw.githubusercontent.com/labaneilers/bs/master/bs
 ERROR="$?"
 if [ ! "$ERROR" = "0" ]; then
     echo "Failed to download"
     exit 1
 fi
 
-chmod +x ~/bin/bs
+chmod +x "$BS_BIN_PATH"
 
 TESTED=$(which bs)
 if [[ "$TESTED" = "" ]]; then
-    echo "bs was installed at ~/bin/bs, but it wasn't on your PATH."
+    echo "bs was installed at $BS_BIN_PATH, but it wasn't on your PATH."
     exit 1
 fi
 
-echo "bs installed successfully at ~/bin/bs"
+echo "bs installed successfully at $BS_BIN_PATH"
